25d6c42db4f3e84e8536eb7bda04831cd9b69c16
AMIT-DUBEY-LTI <Amit.Dubey@lntinfotech.com>
2021-03-23 10:01:39 +0530

    changes did by amit

--- DisplayNames/Program.cs
+++ DisplayNames/Program.cs
@@ -12,10 +12,9 @@ namespace DisplayNames
         {
             Console.WriteLine("This is a Group Project: Display_names");
             Console.WriteLine("My name is Amit Dubey");
+            Console.WriteLine("Hope you all are fine");
             Console.WriteLine("My name is Ankur Rai");
             Console.WriteLine("My name is Azharudeen S");
-            Console.ReadKey();
-
             Console.WriteLine("My name is Aniesha Razdan");
 
             Console.ReadLine();
